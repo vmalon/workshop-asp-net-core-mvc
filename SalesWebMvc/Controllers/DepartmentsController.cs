54654557492b24762bbb659168c90bc8028495ba
vmalon <50339067+vmalon@users.noreply.github.com>
2020-01-02 09:35:16 -0300

    Primeiro teste MVC: Department
    DepartmentsController, Departments - Index

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Instancia uma lista do tipo Department 
+            List<Department> list = new List<Department>();
+
+            list.Add(new Department { Id = 1, Name = "Eletronics" });
+            list.Add(new Department { Id = 2, Name = "Fashion" });
+
+            //E a retorna como parâmtro no método View
+            return View(list);
+        }
+    }
+}
